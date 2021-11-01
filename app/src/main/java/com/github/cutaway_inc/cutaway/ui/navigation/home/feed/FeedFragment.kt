352f7f910951c4dddc59db0f207325233cb6e825
karchx <karch.andrus@gmail.com>
2021-11-01 21:26:55 +0300

    Added hiding keyboard on screen tap

--- app/src/main/java/com/github/cutaway_inc/cutaway/ui/navigation/home/feed/FeedFragment.kt
+++ app/src/main/java/com/github/cutaway_inc/cutaway/ui/navigation/home/feed/FeedFragment.kt
@@ -1,9 +1,11 @@
 package com.github.cutaway_inc.cutaway.ui.navigation.home.feed
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.github.cutaway_inc.cutaway.R
@@ -27,10 +29,25 @@ class FeedFragment : Fragment(R.layout.fragment_users_feed) {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val globalSearchTiText = binding.globalSearchTiText
+        binding.usersFeedScreen.setOnClickListener {
+            hideKeyboard()
+            globalSearchTiText.clearFocus()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun hideKeyboard() {
+        val imm =
+            requireActivity().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        var view = requireActivity().currentFocus
+        if (view == null) {
+            view = View(requireActivity())
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
