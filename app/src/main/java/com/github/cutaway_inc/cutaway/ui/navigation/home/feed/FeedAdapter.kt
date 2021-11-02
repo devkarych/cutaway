b413d9febd7ded44095a43ca9497090761f5cf2d
karchx <karch.andrus@gmail.com>
2021-11-02 20:40:45 +0300

    Added ViewType for recycler's title

--- app/src/main/java/com/github/cutaway_inc/cutaway/ui/navigation/home/feed/FeedAdapter.kt
+++ app/src/main/java/com/github/cutaway_inc/cutaway/ui/navigation/home/feed/FeedAdapter.kt
@@ -17,24 +17,51 @@ class FeedAdapter(
     private val context: Context,
     private val activity: MainActivity,
     private val users: List<UserDtf>
-) : RecyclerView.Adapter<FeedAdapter.FeedViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.user_feed_item, parent, false)
-        return FeedViewHolder(view)
+    private val TYPE_HEADER = 0
+    private val TYPE_ITEM = 1
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        if (viewType == TYPE_ITEM) {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.user_feed_item, parent, false)
+            return FeedViewHolder(view)
+        } else if (viewType == TYPE_HEADER) {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.rv_title_item, parent, false)
+            return TitleViewHolder(view)
+        }
+        throw RuntimeException("there's no type that matches the allowed type.")
     }
 
-    override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
-        val user = users[position].toUser()
-        setImage(holder, _imageId = "my_icon")
-        holder.userUsername.text = "@${user.username}"
-        holder.userFullName.text = user.fullName
-        holder.userFollowers.text = ", ${user.numberOfFollowers} followers"
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is FeedViewHolder) {
+            val user = users[position-1].toUser()
+            setImage(holder, _imageId = "my_icon")
+            holder.userUsername.text = "@${user.username}"
+            holder.userFullName.text = user.fullName
+            holder.userFollowers.text = ", ${user.numberOfFollowers} followers"
+        } else if (holder is TitleViewHolder) {
+            holder.title.text = activity.resources.getText(R.string.feed)
+        }
     }
 
     override fun getItemCount(): Int {
-        return users.size
+        return users.size + 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (isPositionHeader(position)) {
+            return TYPE_HEADER
+        }
+
+        return TYPE_ITEM
+    }
+
+    private fun isPositionHeader(position: Int): Boolean {
+        return position == 0
     }
 
     private fun setImage(holder: FeedViewHolder, _imageId: String) {
@@ -57,5 +84,9 @@ class FeedAdapter(
         val userFullName: TextView = itemView.findViewById(R.id.user_feed_full_name)
         val userUsername: TextView = itemView.findViewById(R.id.user_feed_username)
         val userFollowers: TextView = itemView.findViewById(R.id.user_feed_followers)
+    }
+
+    class TitleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title: TextView = itemView.findViewById(R.id.rv_title_tv)
     }
 }
