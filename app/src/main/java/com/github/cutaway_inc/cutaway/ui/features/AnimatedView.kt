9aad97b9b5cdff4ec51eff42214c0959d00067b8
karchx <karch.andrus@gmail.com>
2021-11-02 21:18:15 +0300

    ScaleAnim updated

--- app/src/main/java/com/github/cutaway_inc/cutaway/ui/features/AnimatedView.kt
+++ app/src/main/java/com/github/cutaway_inc/cutaway/ui/features/AnimatedView.kt
@@ -5,20 +5,17 @@ import android.view.animation.Animation
 
 import android.view.animation.ScaleAnimation
 
-
-
-
 class AnimatedView(private val view: View) {
     fun setScaleAnimation() {
         val anim = ScaleAnimation(
-            0.9f,
+            0.95f,
             1.0f,
-            0.9f,
+            0.95f,
             1.0f,
             Animation.RELATIVE_TO_SELF,
-            0.9f,
+            0.95f,
             Animation.RELATIVE_TO_SELF,
-            0.9f
+            0.95f
         )
         anim.duration = Constants.FADE_DURATION
         view.startAnimation(anim)
