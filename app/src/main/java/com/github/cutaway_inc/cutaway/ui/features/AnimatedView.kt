9f1e0f8212b0c8582f050e12153aad4d8befc8cf
karchx <karch.andrus@gmail.com>
2021-11-02 21:16:21 +0300

    Updated animations

---
+++ app/src/main/java/com/github/cutaway_inc/cutaway/ui/features/AnimatedView.kt
@@ -0,0 +1,30 @@
+package com.github.cutaway_inc.cutaway.ui.features
+
+import android.view.View
+import android.view.animation.Animation
+
+import android.view.animation.ScaleAnimation
+
+
+
+
+class AnimatedView(private val view: View) {
+    fun setScaleAnimation() {
+        val anim = ScaleAnimation(
+            0.9f,
+            1.0f,
+            0.9f,
+            1.0f,
+            Animation.RELATIVE_TO_SELF,
+            0.9f,
+            Animation.RELATIVE_TO_SELF,
+            0.9f
+        )
+        anim.duration = Constants.FADE_DURATION
+        view.startAnimation(anim)
+    }
+}
+
+private object Constants {
+    const val FADE_DURATION = 350L
+}
